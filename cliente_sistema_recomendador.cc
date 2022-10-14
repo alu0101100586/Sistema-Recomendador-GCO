0995cbb90453f54083cc3cb4a8b7137ff07c867e
Jonay_Estevez <alu0101100586@ull.edu.es>
2022-10-14 09:54:32 +0100

    Creacion del fichero que va a tener el programa cliente

---
+++ cliente_sistema_recomendador.cc
@@ -0,0 +1,15 @@
+/**
+ * @file cliente_sistema_recomendador.cc
+ * 
+ * @author Bruno Lorenzo Arroyo Pedraza (pon tu email puto)
+ * @author Carla Cristina Olivares Rodriguez (pon tu email puta)
+ * @author Jose Miguel Hernandez Santana (alu0101101507@ull.edu.es)
+ * @author Jonay Estévez Diaz (alu0101100586@ull.edu.es)
+ * 
+ * @brief Sistema recomendador
+ * @version 0.1
+ * @date 2022-10-14
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
